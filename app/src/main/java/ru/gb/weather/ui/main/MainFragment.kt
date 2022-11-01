5c61738d7c468306c822dfc9b31c4ef56920ece7
DwarfPorter <mrvlmor@gmail.com>
2022-11-01 16:52:34 +0300

    Добавление binding

--- app/src/main/java/ru/gb/weather/ui/main/MainFragment.kt
+++ app/src/main/java/ru/gb/weather/ui/main/MainFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import ru.gb.weather.R
+import ru.gb.weather.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
@@ -16,17 +19,35 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val observer = Observer<Any> { renderData(it) }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(context, "data", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
