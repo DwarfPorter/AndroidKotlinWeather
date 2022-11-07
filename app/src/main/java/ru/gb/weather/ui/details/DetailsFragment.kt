6a20b7fe0ab6117b6f24d093ed3eb396c70aa43b
DwarfPorter <mrvlmor@gmail.com>
2022-11-07 17:38:51 +0300

    Получение погоды из интернета

--- app/src/main/java/ru/gb/weather/ui/details/DetailsFragment.kt
+++ app/src/main/java/ru/gb/weather/ui/details/DetailsFragment.kt
@@ -1,30 +1,40 @@
 package ru.gb.weather.ui.details
 
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import com.google.gson.Gson
+import ru.gb.weather.BuildConfig
 import ru.gb.weather.R
 import ru.gb.weather.databinding.FragmentDetailsBinding
+import ru.gb.weather.model.City
 import ru.gb.weather.model.Weather
+import ru.gb.weather.model.data.WeatherDTO
+import ru.gb.weather.model.data.mapTo
 import ru.gb.weather.ui.main.MainViewModel
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
+
+
+/*
+file apikey.properties
+yandex_weather_api_key = "..."
+*/
 
 class DetailsFragment : Fragment() {
 
-    companion object {
-        const val BUNDLE_EXTRA = "weather"
-
-        fun newInstance(weather: Weather): DetailsFragment {
-            val fragment = DetailsFragment()
-            val bundle = Bundle().also {
-                it.putParcelable(BUNDLE_EXTRA, weather)
-            }
-            fragment.arguments = bundle
-            return fragment
-        }
-    }
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
@@ -41,27 +51,91 @@ class DetailsFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let { weather ->
-            with(binding)
-            {
-                weather.city.also { city ->
-                    cityName.text = city.city
-                    cityCoordinates.text = String.format(
-                        getString(R.string.city_coordinates),
-                        city.lat.toString(),
-                        city.lon.toString()
+        val weather = arguments?.getParcelable(BUNDLE_EXTRA) ?: Weather()
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+        loadWeather(weather.city)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun loadWeather(city: City) {
+        try {
+            val uri =
+                URL("https://api.weather.yandex.ru/v2/informers?lat=${city.lat}&lon=${city.lon}")
+            val handler = Handler(Looper.myLooper()!!)
+            Thread(Runnable {
+                lateinit var urlConnection: HttpsURLConnection
+                try {
+                    urlConnection = uri.openConnection() as HttpsURLConnection
+                    urlConnection.requestMethod = "GET"
+                    urlConnection.addRequestProperty(
+                        "X-Yandex-API-Key",
+                        BuildConfig.WEATHER_API_KEY
                     )
+                    urlConnection.readTimeout = 10000
+                    val bufferedReader =
+                        BufferedReader(InputStreamReader(urlConnection.inputStream))
+
+                    // преобразование ответа от сервера (JSON) в модель данных (WeatherDTO)
+                    val weatherDTO: WeatherDTO =
+                        Gson().fromJson(getLines(bufferedReader), WeatherDTO::class.java)
+                    handler.post { displayWeather(weatherDTO.mapTo(city)) }
+                } catch (e: Exception) {
+                    Log.e("", "Fail connection", e)
+                    e.printStackTrace()
+                    //Обработка ошибки
+                } finally {
+                    urlConnection.disconnect()
                 }
-                temperatureValue.text = weather.temperature.toString()
-                feelsLikeValue.text = weather.feelsLike.toString()
+            }).start()
+        } catch (e: MalformedURLException) {
+            Log.e("", "Fail URI", e)
+            e.printStackTrace()
+            //Обработка ошибки
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getLines(reader: BufferedReader): String {
+        return reader.lines().collect(Collectors.joining("\n"))
+    }
+
+    private fun displayWeather(weather: Weather) {
+        with(binding) {
+            mainView.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            weather.city.also { city ->
+                cityName.text = city.city
+                cityCoordinates.text = String.format(
+                    getString(R.string.city_coordinates),
+                    city.lat.toString(),
+                    city.lon.toString()
+                )
             }
+            weatherCondition.text = weather.condition
+            temperatureValue.text = weather.temperature.toString()
+            feelsLikeValue.text = weather.feelsLike.toString()
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        const val BUNDLE_EXTRA = "weather"
+
+        fun newInstance(weather: Weather): DetailsFragment {
+            val fragment = DetailsFragment()
+            val bundle = Bundle().also {
+                it.putParcelable(BUNDLE_EXTRA, weather)
+            }
+            fragment.arguments = bundle
+            return fragment
+        }
     }
 }
