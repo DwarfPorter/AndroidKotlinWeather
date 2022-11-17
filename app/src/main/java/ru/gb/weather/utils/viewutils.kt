d1a6bd4c458d3b9e2942fed54fdea2ba58111dd2
DwarfPorter <mrvlmor@gmail.com>
2022-11-17 12:04:44 +0300

    Небольшой рефакторинг

---
+++ app/src/main/java/ru/gb/weather/utils/viewutils.kt
@@ -0,0 +1,21 @@
+package ru.gb.weather.utils
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+fun View.showSnackBar(
+    text: String,
+    actionText: String,
+    length: Int = Snackbar.LENGTH_INDEFINITE,
+    action: (View) -> Unit
+) {
+    Snackbar.make(this, text, length).setAction(actionText, action).show()
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE;
+}
