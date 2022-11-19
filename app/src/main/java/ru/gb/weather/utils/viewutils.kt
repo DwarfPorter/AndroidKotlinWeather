7c88c29e7521c17e814f837d5825217e5e7cd5a2
DwarfPorter <mrvlmor@gmail.com>
2022-11-19 19:36:03 +0300

    fix merge

--- app/src/main/java/ru/gb/weather/utils/viewutils.kt
+++ app/src/main/java/ru/gb/weather/utils/viewutils.kt
@@ -3,6 +3,14 @@ package ru.gb.weather.utils
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE;
+}
+
 fun View.showSnackBar(
     text: String,
     actionText: String,
@@ -10,12 +18,4 @@ fun View.showSnackBar(
     action: (View) -> Unit
 ) {
     Snackbar.make(this, text, length).setAction(actionText, action).show()
-}
-
-fun View.show() {
-    this.visibility = View.VISIBLE
-}
-
-fun View.hide() {
-    this.visibility = View.GONE;
 }
